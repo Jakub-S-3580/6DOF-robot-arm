fb86456963506950d7e075d4b89a2c1a0a006bf3
Jakub-S-3580 <jakub_sz@icloud.com>
2024-12-25 19:03:14 +0100

    first python codes

--- BRICK_1/main_brick1.py
+++ BRICK_1/main_brick1.py
@@ -1,5 +1,21 @@
- #!/usr/bin/env python3
-from ev3dev2 import OUTPUT_A,OUTPUT_B,OUTPUT_C,OUTPUT_D, LargeMotor
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import time
+from ev3dev2 import LargeMotor, OUTPUT_A,OUTPUT_B,OUTPUT_C,OUTPUT_D
+
+# state constants
+ON = True
+OFF = False
+
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
 
 A1 = LargeMotor(OUTPUT_A)
-A1.on_for_seconds(100,5,1)
+A1.on_for_seconds(100,3)

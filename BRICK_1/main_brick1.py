20e622d90ed0cf3fbe11b73bc81dfda2551d7c62
Jakub-S-3580 <jakub_sz@icloud.com>
2025-04-14 20:47:27 +0100

    minor changes to mainbrick1

--- BRICK_1/main_brick1.py
+++ BRICK_1/main_brick1.py
@@ -8,14 +8,11 @@ from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
 sys.path.insert(1, '../')
 from general_functions import *
 
-# state constants
-ON = True
-OFF = False
 
 A1 = LargeMotor(OUTPUT_A)
 A2 = LargeMotor(OUTPUT_C)
-Pneumatics = LargeMotor(OUTPUT_D)
 
-run(A1,100,1)
+run(A1,100,3)
+run(A1,-100,3)
 run(A2,-100,3)
-run(Pneumatics,100,3)
+run(A2,100,3)
